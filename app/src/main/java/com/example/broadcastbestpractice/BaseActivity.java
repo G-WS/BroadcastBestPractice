25f12d769a68af1f86962d15aecb71444a2c6b54
G-WS <3355139577@qq.com>
2022-08-09 22:14:03 +0800

    0809

---
+++ app/src/main/java/com/example/broadcastbestpractice/BaseActivity.java
@@ -0,0 +1,64 @@
+package com.example.broadcastbestpractice;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+    private ForceOfflineReceiver receiver;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        Log.d("BaseActivity", getClass().getSimpleName());
+        ActivityCollector.addActivity(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("com.example.broadcastbestpractice.FORCE_OFFLINE");
+        receiver = new ForceOfflineReceiver();
+        registerReceiver(receiver,intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(receiver!=null){
+            unregisterReceiver(receiver);
+            receiver=null;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
+    }
+    class ForceOfflineReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(final Context context, Intent intent) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle("warning");
+            builder.setMessage("you are forced to be offline. Please try to login again.");
+            builder.setCancelable(false);
+            builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    ActivityCollector.finishAll();
+                    Intent intent1 = new Intent(context,LoginActivity.class);
+                    context.startActivity(intent1);
+                }
+            });
+            builder.show();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/broadcastbestpractice/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.broadcastbestpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button forceOffline = (Button) findViewById(R.id.force_offline);
+        forceOffline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("com.example.broadcastbestpractice.FORCE_OFFLINE");
+                intent.setPackage("com.example.broadcastbestpractice");
+                sendBroadcast(intent);
+            }
+        });
+    }
+}
